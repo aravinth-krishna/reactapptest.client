d66dce0574edb4e40e468897bf9dab558ac868d1
Aravinth krishna <r.aravinthkrishna@gmail.com>
2025-11-26 14:52:58 +0530

    Enhance Signin and Signup components with improved styling and added navigation links

--- src/components/auth/Signin.jsx
+++ src/components/auth/Signin.jsx
@@ -1,17 +1,26 @@
 import styles from "./Signin.module.css";
+import { Link } from "react-router-dom";
 
 function Signin() {
   return (
     <div className={styles.signin}>
       <h1>Log In</h1>
-      <p>Enter your credentials to sign in.</p>
-      <form action="">
+      <p>Enter your credentials to access your account.</p>
+      <form>
         <label htmlFor="email">Email</label>
         <input type="email" placeholder="john.doe@example.com" required />
+
         <label htmlFor="password">Password</label>
-        <input type="password" placeholder="........" required />
-        <button>Login</button>
-        <a href="#">Forgot Password?</a>
+        <input type="password" placeholder="********" required />
+
+        <button type="submit">Login</button>
+
+        <div className={styles.extra}>
+          <Link to="/auth/forgot-password">Forgot Password?</Link>
+          <span>
+            Don't have an account? <Link to="/auth/register">Sign Up</Link>
+          </span>
+        </div>
       </form>
     </div>
   );
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,4 +1,5 @@
 import styles from "./Signup.module.css";
+import { Link } from "react-router-dom";
 
 function Signup() {
   return (
@@ -7,16 +8,26 @@ function Signup() {
       <p>
         Join Learnit to optimize your study routines and track your progress.
       </p>
-      <form action="">
+      <form>
         <label htmlFor="fullName">Full Name</label>
         <input type="text" placeholder="John Doe" required />
+
         <label htmlFor="email">Email</label>
         <input type="email" placeholder="john.doe@example.com" required />
+
         <label htmlFor="password">Password</label>
-        <input type="password" placeholder="........" required />
-        <label htmlFor="password">Confirm Password</label>
-        <input type="password" placeholder="........" required />
-        <button>Create Account</button>
+        <input type="password" placeholder="********" required />
+
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input type="password" placeholder="********" required />
+
+        <button type="submit">Create Account</button>
+
+        <div className={styles.extra}>
+          <span>
+            Already have an account? <Link to="/auth/login">Sign In</Link>
+          </span>
+        </div>
       </form>
     </div>
   );
